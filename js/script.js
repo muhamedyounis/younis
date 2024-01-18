1a22da2aa03144d2006c6d84d294d0b10b395dfc
muhamedyounis <muhamad.ashraf.younis@gmail.com>
2024-01-18 11:35:12 +0200

    bs

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var subject = document.getElementById("subject");
 var message = document.getElementById("message");
 var submitBtn = document.getElementById("submit");
 var contactForm = document.getElementsByTagName("form");
+
 function sendEmail() {
   const messageBody = ` Name: ${userName.value} <br/> Email: ${email.value} <br/> Subject: ${subject.value} <br/> Message: ${message.value} `;
   Email.send({
@@ -24,14 +25,59 @@ function sendEmail() {
   });
 }
 
+var nameRegex = /^[a-z ,.'-]+$/i;
+var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+var subjectRegex = /^[a-z ,.'-]+$/i;
+var messageRegex = /^[a-z ,.'-0123456789]+$/;
+
+userName.addEventListener("keyup", () => {
+  if (nameRegex.test(userName.value)) {
+    userName.classList.remove("is-invalid");
+    userName.classList.add("is-valid");
+  } else {
+    userName.classList.remove("is-valid");
+    userName.classList.add("is-invalid");
+  }
+});
+
+email.addEventListener("keyup", () => {
+  if (emailRegex.test(email.value)) {
+    email.classList.remove("is-invalid");
+    email.classList.add("is-valid");
+  } else {
+    email.classList.remove("is-valid");
+    email.classList.add("is-invalid");
+  }
+});
+
+subject.addEventListener("keyup", () => {
+  if (subjectRegex.test(subject.value)) {
+    subject.classList.remove("is-invalid");
+    subject.classList.add("is-valid");
+  } else {
+    subject.classList.remove("is-valid");
+    subject.classList.add("is-invalid");
+  }
+});
+
+message.addEventListener("keyup", () => {
+  if (messageRegex.test(message.value)) {
+    message.classList.remove("is-invalid");
+    message.classList.add("is-valid");
+  } else {
+    message.classList.remove("is-valid");
+    message.classList.add("is-invalid");
+  }
+});
+
 contactForm[0].addEventListener("submit", (e) => {
   e.preventDefault();
-  sendEmail();
+  // if () {
+  //   sendEmail();
+  // }
 });
 
 var serviceCta = document.querySelectorAll(".single-skill .cta");
-
-console.log(serviceCta);
 
 serviceCta[0].addEventListener("click", () => {
   subject.value = "Web Development - ";
